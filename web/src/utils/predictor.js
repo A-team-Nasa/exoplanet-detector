8ffd896f42784857b729dad5bd1731b34de9bbf7
Riccino <riccardorinojosam@gmail.com>
2025-10-04 19:39:46 -0300

    Modificado endpoint en frontend

--- web/src/utils/predictor.js
+++ web/src/utils/predictor.js
@@ -11,7 +11,7 @@ export const predictSingleObject = async (data) => {
   // que tenga el modelo XGBoost cargado
   
   try {
-    const response = await fetch('/api/predict', {
+    const response = await fetch('http://127.0.0.1:8000/predict', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
